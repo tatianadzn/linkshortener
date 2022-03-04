56ee270e39e9b2fb916789e28e4d24b576915d84
tatianadzn <tatiana22d@gmail.com>
2022-03-04 22:05:48 +0300

    Short link generator implemented

--- src/main/java/com/nordcodes/testassignment/linkshortener/utils/ShortLinkGenerator.java
+++ src/main/java/com/nordcodes/testassignment/linkshortener/utils/ShortLinkGenerator.java
@@ -1,8 +1,19 @@
 package com.nordcodes.testassignment.linkshortener.utils;
 
+import java.security.SecureRandom;
+
 public class ShortLinkGenerator {
 
+    private static final int SHORT_LINK_LENGTH = 6;
+    private static final String ALPHABET = "abcdefghigklmnopqrstuvwxyz0123456789";
+
     public static String generate() {
-        return "1";
+        final SecureRandom random = new SecureRandom();
+        char[] chars = new char[SHORT_LINK_LENGTH];
+        for (int i = 0; i < chars.length; i++) {
+            int randomPosition = random.nextInt(ALPHABET.length());
+            chars[i] = ALPHABET.charAt(randomPosition);
+        }
+        return String.valueOf(chars);
     }
 }
